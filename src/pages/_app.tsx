a701fdaaaf2a6e3dc9f65c67a7e17d8660d5870a
Brayden <Braydenlwong@gmail.com>
2023-03-03 11:12:59 -0700

    added github action for the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,9 @@ import { type AppType } from "next/app";
 import "../styles/globals.css";
 import "../styles/profilestyles.css";
 import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
-  return (
-    <GoogleOAuthProvider clientId="567585874111-4mbeosqdl6sht4m6vrpnpgcd5ti0r78c.apps.googleusercontent.com">
-      <Component {...pageProps} />
-    </GoogleOAuthProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
