8c85316b381e68a1f5896c9053310efbe0e363e3
armando-ai <aramirezcanas@student.neumont.edu>
2023-03-04 11:01:04 -0700

    fixed

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -17,7 +17,7 @@ export const Navigation = (props: any) => {
   const [selected, setSelected] = useState("Home");
   const [openChats, setOpenChats] = useState(false);
   const date = new Date();
-
+  let socket: any;
   const [chatRoom, setChatRoom] = useState<any>([]);
   useEffect(() => {
     let socket = io(`${process.env.NEXT_PUBLIC_WS_URL}`, {
@@ -26,18 +26,24 @@ export const Navigation = (props: any) => {
       },
     });
 
+    socket.on("createdRoom", (data: any) => {
+      console.log("createRoom", data);
+      setChatRoom((prev: any) => [
+        <ChatRoom delRoom={setChatRoom} room={data}></ChatRoom>,
+      ]);
+      socket.emit("joinRooms")
+    });
 
     socket.on("joinedRooms", (data: any) => {
       console.log("joined??? -" + data);
     });
-    socket.on("connected", (data) => {
-      
-    });
+
 
     return () => {
       socket.disconnect();
     };
   }, []);
+
 
   const [chats, setChat] = useState([
     {
