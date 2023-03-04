8eaab4223632b111cfcd16219f6c8fec35636939
Brayden <Braydenlwong@gmail.com>
2023-03-03 18:32:41 -0700

    updated request endpoint

--- src/utils/functions/get.token.ts
+++ src/utils/functions/get.token.ts
@@ -1,41 +1,42 @@
-import { request } from './request';
-import { Response } from '../types';
+import { request } from "./request";
+import { Response } from "../types";
 
 type RefreshResponse = {
   accessToken: string;
   refreshToken: string;
-}
+};
 
 type RefreshError = {
   message: string;
   statusCode: number;
-  status: 'error';
+  status: "error";
 };
 
 export const getToken = async () => {
-  const at = localStorage.getItem('accessToken');
-  const rt = localStorage.getItem('refreshToken');
+  const at = localStorage.getItem("accessToken");
+  const rt = localStorage.getItem("refreshToken");
 
-  if(!at || !rt) {
+  if (!at || !rt) {
     return null;
   }
 
-  const refreshResponse = await request<Response<RefreshResponse, RefreshError>>('/auth/refresh', {
-    method: 'post',
+  const refreshResponse = await request<
+    Response<RefreshResponse, RefreshError>
+  >("/auth/refresh", {
+    method: "post",
     headers: {
-      "authorization": `Bearer ${rt}`,
-      "Content-Type": "application/json"
-    }
+      authorization: `Bearer ${rt}`,
+      "Content-Type": "application/json",
+    },
   });
 
-
-  if(refreshResponse.status === 'error') {
+  if (refreshResponse.status === "error") {
     return null;
   }
 
   const { accessToken, refreshToken } = refreshResponse.data;
-  localStorage.setItem('accessToken', accessToken);
-  localStorage.setItem('refreshToken', refreshToken);
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
 
   return [accessToken] as const;
-}
+};
--- src/utils/functions/request.ts
+++ src/utils/functions/request.ts
@@ -18,7 +18,7 @@ export const request = async <T>(
 ) => {
   const { method, headers, body } = options;
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
     method: method.toUpperCase(),
     headers: headers,
     body: JSON.stringify(body),
